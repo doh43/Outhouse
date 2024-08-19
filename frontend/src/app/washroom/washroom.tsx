c3b47f3b6db32625b8625dd8ccf438bf2aa043b1
Taejun Ha <Taejun Ha>
2024-08-19 12:20:14 -0400

    created view washroom feature

---
+++ frontend/src/app/washroom/washroom.tsx
@@ -0,0 +1,13 @@
+// pages/washroom.tsx
+import WashroomLayout from '@/app/washroom/layout';
+import WashroomPage from '@/app/washroom/page';
+
+const Washroom = () => {
+  return (
+    <WashroomLayout>
+      <WashroomPage />
+    </WashroomLayout>
+  );
+};
+
+export default Washroom;
--- frontend/src/components/home/InfoWindowContent.tsx
+++ frontend/src/components/home/InfoWindowContent.tsx
@@ -1,14 +1,17 @@
+'use client';
 import React from 'react';
 import StarRating from './StarRating';
+import Link from "next/link";
 
 interface InfowindowContentProps {
   name: string;
+  id: string;
   description: string;
   averageRating: number;
   coordinates: number[];
 }
 
-const InfowindowContent: React.FC<InfowindowContentProps> = ({ name, description, averageRating, coordinates }) => {
+const InfowindowContent: React.FC<InfowindowContentProps> = ({ name, id, description, averageRating, coordinates }) => {
   const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${coordinates[0]},${coordinates[1]}`;
 
   return (
@@ -16,17 +19,34 @@ const InfowindowContent: React.FC<InfowindowContentProps> = ({ name, description
       <h1 className="text-xl font-bold mb-2">{name}</h1>
       <p className="text-sm xs:text-xs text-gray-700 mb-4">{description}</p>
       <div className="flex flex-col space-y-2">
-        <a target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 underline transition duration-300">
+        <Link
+          href={{
+            pathname: '/washroom',
+            query: {
+              washroomId: id,
+              name: name,
+              description: description,
+              averageRating: averageRating,
+              coordinates: JSON.stringify(coordinates)
+            },
+          }}
+          className="text-blue-500 hover:text-blue-700 underline transition duration-300"
+        >
           View Washroom
-        </a>
-        </div>
+        </Link>
+      </div>
       <div className="flex flex-col space-y-2">
-        <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 underline transition duration-300">
+        <a
+          href={googleMapsUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:text-blue-700 underline transition duration-300"
+        >
           Open in Google Maps
         </a>
-      <p className="text-sm xs:text-xs text-gray-900 mb-4">
-        <span className="font-semibold"></span> <StarRating rating={averageRating} />
-      </p>
+        <p className="text-sm xs:text-xs text-gray-900 mb-4">
+          <span className="font-semibold"></span> <StarRating rating={averageRating} />
+        </p>
       </div>
     </div>
   );
